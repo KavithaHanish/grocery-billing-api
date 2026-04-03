b52ca2baa62c254dc47beade4ffe12e40b064006
KavithaHanish <kavitha20922@gmail.com>
2026-04-03 11:43:57 +0530

    Added ApplicationDbContext.cs file

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,33 +1,38 @@
 using Microsoft.EntityFrameworkCore;
 using GroceryBillingAPI.Models;
 
-namespace GroceryBillingAPI.Data {
-    public class ApplicationDbContext : DbContext {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-
+namespace GroceryBillingAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
         public DbSet<GroceryProduct> GroceryProducts { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<GroceryProduct>(entity => {
+            modelBuilder.Entity<GroceryProduct>(entity =>
+            {
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.Name)
-                      .IsRequired()
-                      .HasMaxLength(100);
+                    .IsRequired()
+                    .HasMaxLength(100);
                 entity.Property(e => e.Category)
-                      .IsRequired()
-                      .HasMaxLength(50);
+                    .IsRequired()
+                    .HasMaxLength(50);
                 entity.Property(e => e.PricePerKg)
-                      .HasPrecision(10, 2);
+                    .HasPrecision(10, 2);
                 entity.Property(e => e.PurchasePrice)
-                      .HasPrecision(10, 2);
+                    .HasPrecision(10, 2);
                 entity.Property(e => e.StockQuantity)
-                      .HasPrecision(10, 3);
+                    .HasPrecision(10, 3);
                 entity.Property(e => e.Unit)
-                      .IsRequired()
-                      .HasMaxLength(20);
+                    .IsRequired()
+                    .HasMaxLength(20);
                 entity.Property(e => e.CreatedDate)
-                      .HasDefaultValueSql("GETUTCDATE()");
+                    .HasDefaultValueSql("GETUTCDATE()");
             });
         }
     }
