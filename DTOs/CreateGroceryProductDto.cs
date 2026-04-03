549e50d5da910db34b88f4b458dd8b44f327c23d
KavithaHanish <kavitha20922@gmail.com>
2026-04-03 12:00:59 +0530

    Add CreateGroceryProductDto

---
+++ DTOs/CreateGroceryProductDto.cs
@@ -0,0 +1,7 @@
+// DTO for creating a grocery product
+public class CreateGroceryProductDto
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Category { get; set; }
+}
