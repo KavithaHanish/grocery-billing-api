1eb327092b21650b07526fbb9f2effdf94a8b8f2
KavithaHanish <kavitha20922@gmail.com>
2026-04-03 11:42:23 +0530

    Create ApplicationDbContext.cs file

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using GroceryBillingAPI.Models;
+
+namespace GroceryBillingAPI.Data {
+    public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<GroceryProduct> GroceryProducts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<GroceryProduct>(entity => {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name)
+                      .IsRequired()
+                      .HasMaxLength(100);
+                entity.Property(e => e.Category)
+                      .IsRequired()
+                      .HasMaxLength(50);
+                entity.Property(e => e.PricePerKg)
+                      .HasPrecision(10, 2);
+                entity.Property(e => e.PurchasePrice)
+                      .HasPrecision(10, 2);
+                entity.Property(e => e.StockQuantity)
+                      .HasPrecision(10, 3);
+                entity.Property(e => e.Unit)
+                      .IsRequired()
+                      .HasMaxLength(20);
+                entity.Property(e => e.CreatedDate)
+                      .HasDefaultValueSql("GETUTCDATE()");
+            });
+        }
+    }
+}
